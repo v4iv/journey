76e01daf0ff9fbb07b3e222ee7ef9484da81701f
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2023-08-17 01:19:14 +0530

    feat: manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,53 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'icon-192-maskable.png',
+        'icon-512-maskable.png',
+      ],
+      manifest: {
+        name: 'Journey',
+        short_name: 'Journey',
+        description: 'Begin your journey...',
+        theme_color: '#020817',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/icon-512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
