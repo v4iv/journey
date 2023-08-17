f5ea0b75fa52ed6d4aedf8965867a9b247945987
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2023-08-17 20:26:47 +0530

    feat: remove gtag

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,11 @@ import path from 'path'
 import {defineConfig} from 'vite'
 import {VitePWA} from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
-import {VitePluginRadar} from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePluginRadar({
-      // Google Analytics tag injection
-      analytics: {
-        id: process.env.VITE_GTAG,
-      },
-    }),
     VitePWA({
       includeAssets: [
         'splash.png',
